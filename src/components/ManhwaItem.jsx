47d0541827a5c61b28a3994153a5733369ef1555
Théo Zwaenepoel <theozwaenepoel@gmail.com>
2026-03-20 23:00:32 +0100

    Reglage bug affichage

--- src/components/ManhwaItem.jsx
+++ src/components/ManhwaItem.jsx
@@ -29,7 +29,7 @@ const ManhwaItem = ({id, title, chapter, status, lastRead, nsfw, cover, lastRead
                     {nsfw===1 && <span className="nsfw">18+</span>}
                     <div className='info-hover'>
                         {chapter !== "" && <span>Ch. {chapter}</span>}
-                        {lastReadCount !== "" && <span>{lastReadCompter} jours</span>}
+                        {lastRead !== "" && lastReadCount !== "" && <span>{lastReadCompter} jours</span>}
                     </div>
                 </div>
             </li>
