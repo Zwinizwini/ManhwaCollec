5e6b8ef5e82d6b229384fb214b761645d1a1103a
Théo Zwaenepoel <theozwaenepoel@gmail.com>
2026-04-11 23:26:08 +0200

    1

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -21,7 +21,7 @@ const Sort = ({trie, setTrie}) => {
             <option value="0">Par Default</option>
             <option value="1">Note ↓</option>
             <option value="2">Note ↑</option>
-            <option value="3">Nb Chap</option>
+            <option value="3">Chap ↓</option>
         </select>
     )
 }
